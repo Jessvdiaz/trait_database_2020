af77a77c7f73c37ab5b329e88b39bd517a0a5690
Climate Change <climchange@AckPro-2.local>
2020-01-23 17:03:16 -0800

    1st commit

--- CalWoodyTraits_2020project/scripts/Rsandbox.R
+++ CalWoodyTraits_2020project/scripts/Rsandbox.R
@@ -1,11 +1,22 @@
+
 ## R sandbox
 
-## Merging species lists
-x <- data.frame(species=c('a','b','c'),traitx=c(1,2,3),traity=c(10,12,14))
-y <- data.frame(species=c('b','c','d'),traitx=c(2,3,4),traitz=c(100,101,102))
-x
-y
-xy <- merge(x,y,by = 'species',all = T)
+
+NB<- read.csv("north_bay_species_list.csv",header=T)
+PWD <- read.csv("PWD_woody_veg.csv",header=T)
+
+# filter data
+head(NB)
+head(PWD)
+ names(NB)
+ names(PWD)
+
+## Merging data frames 
+# x <- data.frame(species=c('a','b','c'),traitx=c(1,2,3),traity=c(10,12,14))
+# y <- data.frame(species=c('b','c','d'),traitx=c(2,3,4),traitz=c(100,101,102))
+# x
+# y
+ (both <-merge(NB,PWD,all = T))
 xy
 
 ## checking names against a master list
