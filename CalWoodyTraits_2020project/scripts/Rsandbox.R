b7f6f39c45b00dea94d23d349dfe48b497ad9fed
David Ackerly <dackerly@berkeley.edu>
2020-02-07 10:08:35 -0800

    cleaned up the species list files and the initial merge function to create a master list

--- CalWoodyTraits_2020project/scripts/Rsandbox.R
+++ CalWoodyTraits_2020project/scripts/Rsandbox.R
@@ -1,16 +1,28 @@
-
 ## R sandbox
+rm(list=ls()) # erase everything in current environment
+getwd() # get working directory
 
+splist_dir <- "CalWoodyTraits_2020project/data/species_lists/"
+CF <- read.csv(paste(splist_dir,"chilefornia_species.csv",sep=""),header=T,as.is=T)
+PWD <- read.csv("CalWoodyTraits_2020project/data/species_lists/PWD_woody_veg.csv",header=T,as.is=T,na.strings = "NA")
 
-NB<- read.csv("north_bay_species_list.csv",header=T)
-PWD <- read.csv("PWD_woody_veg.csv",header=T)
+# remove NA species - subset worked!!
+dim(PWD)
+PWD <- subset(PWD,!is.na(PWD$CF_species_name))
+dim(PWD)
+
+# remove non-California species from CF and create CA
+dim(CF)
+CA <- subset(CF,!is.na(CF$chilefornia_california))
+dim(CA)
+rm('CF')
 
 # filter data
-head(NB)
+head(CA)
 head(PWD)
 
- names(NB)
- names(PWD)
+names(CA)
+names(PWD)
 
 ## Merging data frames example #####
 # x <- data.frame(species=c('a','b','c'),traitx=c(1,2,3),traity=c(10,12,14))
@@ -21,8 +33,22 @@ head(PWD)
 ###############################
 #### Merging species List #####
  ##############################
- 
-(both<- merge(NB,PWD,by.x="species", by.y="SPECIES",all=T))
+
+#Are all PWD species in CF list?
+PWD_in_CA <- PWD$CF_species_name %in% CA$species
+
+# print species that are present
+PWD$CF_species_name[PWD_in_CA]
+
+# print species that are absent
+PWD$CF_species_name[!PWD_in_CA]
+
+sp_list <- merge(CA,PWD,by.x="species", by.y="CF_species_name",all=T)
+
+names(sp_list)
+
+# look at a record that came in from PWD for a species that was not in the CA tree list - see NAs for the columns from the original CA file
+sp_list[sp_list$species=='Ceanothus cuneatus',]
 
 
 
